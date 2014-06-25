7a7072dc81b4a5d0df12f725195e6a48d866c052
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-06-25 03:49:35 +0300

    clean the project

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 (function($) {
-	var APP = {
-		init: function() {
-			this.userActions();
-		},
-		
-		userActions: function() {
-			$('#connectFB').on('click', function() {
-				this.removeWelcome();
-				
-			});
-			
-			$('#signupQB').on('click', function() {
-				this.removeWelcome();
-				
-			});
-			
-			$('#loginQB').on('click', function() {
-				this.removeWelcome();
-				
-			});
-		},
-		
-		removeWelcome: function() {
-			$('body').removeClass('is-welcome');
-		}
-	};
-	
-	APP.init();
+  var APP = {
+    init: function() {
+      this.userActions();
+    },
+    
+    userActions: function() {
+      $('#connectFB').on('click', function() {
+        this.removeWelcome();
+        
+      });
+      
+      $('#signupQB').on('click', function() {
+        this.removeWelcome();
+        
+      });
+      
+      $('#loginQB').on('click', function() {
+        this.removeWelcome();
+        
+      });
+    },
+    
+    removeWelcome: function() {
+      $('body').removeClass('is-welcome');
+    }
+  };
+  
+  APP.init();
 })(jQuery);
