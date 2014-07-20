23db33d60004903fc7aa7b211270b858057d9c5d
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-07-21 01:34:14 +0300

    refactor FriendList

--- js/models/friend_list.js
+++ js/models/friend_list.js
@@ -5,51 +5,52 @@
  *
  */
 
-var Contact = require('../contacts/ContactModel'),
-    QBApiCalls = require('../qbApiCalls');
+module.exports = FriendList;
 
-module.exports = Friendlist;
-
-function Friendlist() {
-  this.contacts = [];
+function FriendList(app) {
+  this.app = app;
 }
 
-Friendlist.prototype.globalSearch = function(callback) {
-  var val = sessionStorage['QM.search.value'],
-      page = sessionStorage['QM.search.page'],
-      self = this;
+FriendList.prototype = {
   
-  QBApiCalls.getUser({full_name: val, page: page}, function(data) {
-    sessionStorage.setItem('QM.search.allPages', Math.ceil(data.total_entries / data.per_page));
-    sessionStorage.setItem('QM.search.page', ++page);
+  globalSearch: function(callback) {
+    var val = sessionStorage['QM.search.value'],
+        page = sessionStorage['QM.search.page'],
+        self = this;
     
-    self.getContacts(data.items);
-    if (QMCONFIG.debug) console.log('Search results', self);
+    QBApiCalls.getUser({full_name: val, page: page}, function(data) {
+      sessionStorage.setItem('QM.search.allPages', Math.ceil(data.total_entries / data.per_page));
+      sessionStorage.setItem('QM.search.page', ++page);
+      
+      self.getContacts(data.items);
+      if (QMCONFIG.debug) console.log('Search results', self);
 
-    callback();
-  });
-};
+      callback();
+    });
+  },
 
-Friendlist.prototype.getContacts = function(data) {
-  var self = this,
-      contact;
-  
-  self.contacts = [];
-  data.forEach(function(item) {
-    contact = new Contact(item.user);
-    self.contacts.push(contact);
-  });
-};
+  getContacts: function(data) {
+    var self = this,
+        contact;
+    
+    self.contacts = [];
+    data.forEach(function(item) {
+      contact = new Contact(item.user);
+      self.contacts.push(contact);
+    });
+  },
 
-Friendlist.prototype.sendSubscribe = function(jid) {
-  var user = JSON.parse(localStorage['QM.user']).contact;
-  var extension = {
-    full_name: user.full_name,
-    avatar_url: user.avatar_url
-  };
-  QBApiCalls.subscriptionPresence({jid: jid, type: 'subscribe', extension: extension});
-};
+  sendSubscribe: function(jid) {
+    var user = JSON.parse(localStorage['QM.user']).contact;
+    var extension = {
+      full_name: user.full_name,
+      avatar_url: user.avatar_url
+    };
+    QBApiCalls.subscriptionPresence({jid: jid, type: 'subscribe', extension: extension});
+  },
 
-Friendlist.prototype.sendReject = function(jid) {
-  QBApiCalls.subscriptionPresence({jid: jid, type: 'unsubscribed'});
+  sendReject: function(jid) {
+    QBApiCalls.subscriptionPresence({jid: jid, type: 'unsubscribed'});
+  }
+
 };
