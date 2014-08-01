89434c66146c675bce18543efd210ba0315b864f
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-08-01 14:02:23 +0300

    fix request links

--- js/routes.js
+++ js/routes.js
@@ -200,6 +200,10 @@ Routes.prototype = {
       DialogView.htmlBuild($(this));
     });
 
+    $('#requestsList').on('click', '.contact', function(event) {
+      event.preventDefault();
+    });
+
     $('.l-workspace-wrap').on('keydown', '.l-message', function(event) {
       var shiftKey = event.shiftKey,
           code = event.keyCode; // code=27 (Esc key), code=13 (Enter key)
