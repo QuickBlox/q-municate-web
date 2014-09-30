12a14e4200b2ecc42dc9d222d9f22767c181b6e1
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-09-30 14:57:09 +0300

    fix counter

--- main.js
+++ main.js
@@ -3096,6 +3096,9 @@ DialogView.prototype = {
       chat.removeClass('is-hidden').siblings().addClass('is-hidden');
       $('.l-chat:visible .scrollbar_message').mCustomScrollbar('destroy');
       self.messageScrollbar();
+      
+      console.log(2222222);
+      console.log(self.app.models.ContactList.dialogs[dialog_id]);
 
       if (typeof dialog.message !== 'undefined') {
         Message.update(dialog.message.join(), dialog_id);
@@ -3562,8 +3565,11 @@ MessageView.prototype = {
     if ((notification_type !== '6' || msg.sender_id !== User.contact.id) && chat.is(':visible'))
       Message.update(msg.id, dialog_id);
     else if (!chat.is(':visible') && chat.length > 0) {
+      console.log(11111111);
+      console.log(self.app.models.ContactList.dialogs[dialog_id]);
       msgArr = dialogs[dialog_id].messages || [];
       dialogs[dialog_id].messages = msgArr.push(msg.id);
+      console.log(self.app.models.ContactList.dialogs[dialog_id]);
     }
 
     if (!chat.is(':visible') && dialogItem.length > 0 && notification_type !== '1') {
