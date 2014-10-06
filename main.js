c0f1c0c97219c636f670b4315fed2c73cba16818
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-10-07 01:08:47 +0300

    QMW-59 fix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*
  * Q-municate chat application
  *
@@ -3983,6 +3983,8 @@ UserView.prototype = {
         if ($(this).find('.list-item').length > 0)
           $(this).removeClass('is-hidden');
       });
+      if ($('.l-list-wrap section:not(#requestsList):not(#searchList) .list-item').length === 0)
+        $('#emptyList').removeClass('is-hidden');
     }
   },
 
@@ -4039,4 +4041,4 @@ var appearAnimation = function() {
   $('.popover:not(.popover_smile)').show(150);
 };
 
-},{}]},{},[1])
+},{}]},{},[1]);
