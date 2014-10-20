9480267a7651f2b54aab447effacdfdca5004027
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-10-20 12:22:06 +0300

    leave chat fix

--- main.js
+++ main.js
@@ -689,8 +689,9 @@ Dialog.prototype = {
       full_name: User.contact.full_name,
     }});
 
-    QB.chat.muc.leave(dialog.room_jid, function() {});
-    QBApiCalls.updateDialog(dialog.id, {pull_all: {occupants_ids: [User.contact.id]}}, function() {});
+    QBApiCalls.updateDialog(dialog.id, {pull_all: {occupants_ids: [User.contact.id]}}, function() {
+      QB.chat.muc.leave(dialog.room_jid, function() {});
+    });
     
     callback();
   }
