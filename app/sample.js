3332a2e8a4c758a9746efdf2c415183c93966299
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-12-17 09:10:33 +0200

    added new images

---
+++ app/sample.js
@@ -0,0 +1,35 @@
+var params = {
+  audio: true,
+  video: true,
+  elemId: 'localStream',
+  options: {
+    muted: true,
+    mirror: true
+  }
+};
+
+webrtc.getUserMedia(params, function(stream, err) {
+  if (stream) {
+    console.log(stream);
+    webrtc.attachMediaStream('remoteStream', stream, {muted:true});
+  } else {
+    console.log(err);
+  }
+});
+
+var video = document.getElementById('localStream');
+if (video) {
+  video.addEventListener('timeupdate', function() {
+    var time = Math.floor(video.currentTime),
+        h, min, sec;
+
+    h = Math.floor( time / 3600 );
+    h = h >= 10 ? h : '0' + h;
+    min = Math.floor( time / 60 );
+    min = min >= 10 ? min : '0' + min;
+    sec = Math.floor( time % 60 );
+    sec = sec >= 10 ? sec : '0' + sec;
+
+    $('.mediacall-info-duration, .mediacall-remote-duration').text(h + ':' + min + ':' + sec);
+  });
+}
