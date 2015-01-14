9ffcb8e332163ebe25499ad20492849b5e9a1b7b
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2015-01-14 17:51:52 +0200

    fix facebook connection render into profile view

--- app/scripts/views/profile.js
+++ app/scripts/views/profile.js
@@ -23,17 +23,7 @@ define([
     },
 
     render: function() {
-      var isFacebookUser = this.model.get('facebook_id');
       var template = this.$el.html( this.template(this.model.toJSON()) );
-      if (isFacebookUser) {
-        template.find('.userProfile-field-facebook').html(
-          '<span class="userDetails-label">Facebook:</span><span class="userProfile-facebook">Connected</span>'
-        );
-      } else {
-        template.find('.userProfile-field-facebook').html(
-          '<span class="userDetails-label">Facebook:</span><span class="userProfile-facebook">Not connected</span><button class="btn_userProfile btn_userProfile_connect">Connect</button>'
-        );
-      }
       $('.popups').append(template);
       return this;
     },
