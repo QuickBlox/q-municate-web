c6e797ed5a4d9d7727f721ac781e28095ca139bd
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2015-01-14 03:14:11 +0200

    render profile

--- app/scripts/views/profile.js
+++ app/scripts/views/profile.js
@@ -18,6 +18,8 @@ define([
     id: 'popupProfile',
     className: 'popup popup_profile',
 
+    template: _.template( $('#templateProfile').html() ),
+
     initialize: function(params) {
       if (typeof params === 'object') {
         App = params.app;
@@ -25,8 +27,8 @@ define([
     },
 
     render: function() {
-      var html = '<div class="userDetails-preview"><div class="userDetails-avatar"></div><div class="userDetails-info"><h3 class="userProfile-filename-wrap"><span class="userProfile-filename editable-profile" contenteditable="true"></span><span class="userProfile-header-pencil"></span></h3><div style="clear:both"></div><button class="btn_userProfile btn_userProfile_photo">Set Profile Photo</button><button class="btn_userProfile btn_changePassword">Change password</button><input class="btn_userProfile_file" type="file" accept="image/*"></div></div><div class="userProfile-chatStatus">Status</div><div class="userProfile-status-field userProfile-edit"><span class="userProfile-status-val editable-profile" contenteditable="true"></span></div><div class="userProfile-field"><span class="userDetails-label">Email:</span><span class="userProfile-email"></span></div><div class="userProfile-field userProfile-edit"><span class="userDetails-label">Phone:</span><span class="userProfile-phone editable-profile" contenteditable="true"></span></div><div class="userProfile-field userProfile-field-facebook"></div><div class="userProfile-errors"></div>';
-      $('.popups').append( this.$el.html(html) );
+      var template = this.$el.html( this.template() );
+      $('.popups').append(template);
       return this;
     },
 
