661cfe24ccf78f121296294dcd5a7bf3b31232a1
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2015-01-15 16:36:42 +0200

    fixes of profile UI

--- app/scripts/views/profile.js
+++ app/scripts/views/profile.js
@@ -22,14 +22,23 @@ define([
       
     },
 
+    events: {
+      'click .userProfile-field_phone': 'editPhone'
+    },
+
     render: function() {
       var template = this.$el.html( this.template(this.model.toJSON()) );
       $('.popups').append(template);
+      this.delegateEvents(this.events);
       return this;
     },
 
     openPopup: function() {
       this.$el.add('.popups').addClass('is-overlay');
+    },
+
+    editPhone: function() {
+      this.$el.find('.userProfile-phone').focus();
     }
   });
 
