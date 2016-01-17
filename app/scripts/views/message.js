34fd5c78b7887fac55229696f0da64b4c5596b62
Mary <marymaffka@gmail.com>
2016-01-17 22:10:00 +0200

    html for attach after get blob

--- app/scripts/views/message.js
+++ app/scripts/views/message.js
@@ -51,7 +51,7 @@ define(['jquery', 'config', 'quickblox', 'underscore', 'minEmoji', 'timeago'],
             contact = message.sender_id === User.contact.id ? User.contact : contacts[message.sender_id],
             type = message.notification_type || (message.callState && (parseInt(message.callState) + 7).toString()) || 'message',
             attachType = message.attachment && message.attachment['content-type'] || null,
-            attachUrl = null,
+            attachUrl = message.attachment.public ? QB.content.publicUrl(message.attachment.uid) : QB.content.privateUrl(message.attachment.uid) || null,
             recipient = contacts[recipientId] || null,
             occupants_names = '',
             occupants_ids,
@@ -406,7 +406,7 @@ define(['jquery', 'config', 'quickblox', 'underscore', 'minEmoji', 'timeago'],
 
     onMessage: function(id, message) {
       if (message.type === 'error') return true;
-
+;
       var DialogView = self.app.views.Dialog,
           hiddenDialogs = sessionStorage['QM.hiddenDialogs'] ? JSON.parse(sessionStorage['QM.hiddenDialogs']) : {},
           dialogs = ContactList.dialogs,
@@ -431,12 +431,6 @@ define(['jquery', 'config', 'quickblox', 'underscore', 'minEmoji', 'timeago'],
       msg = Message.create(message);
       msg.sender_id = id;
 
-      if (attach != null) {
-        QB.content.getInfo(attach[0].id, function(err,res) {
-          if (res) blobObj = res.blob;console.log(blobObj);
-        });
-      }
-      console.log(blobObj);
       if ((!deleted_id || msg.sender_id !== User.contact.id) && chat.is(':visible')) {
         Message.update(msg.id, dialog_id);
       } else if (!chat.is(':visible') && chat.length > 0) {
@@ -519,7 +513,20 @@ define(['jquery', 'config', 'quickblox', 'underscore', 'minEmoji', 'timeago'],
       }
 
       if (QMCONFIG.debug) console.log(msg);
-      self.addItem(msg, true, true, id);
+
+      if (attach != null) {
+        QB.content.getInfo(attach[0].id, function(err,res) {
+          if (res) {
+            console.log(res);
+            msg.attachment = res.items[0];
+            console.log(msg);
+            msg.attachment = res.blob;
+            self.addItem(msg, true, true, id);
+          } 
+        });
+      } else {
+        self.addItem(msg, true, true, id);
+      }
 
       if (msg.sender_id !== User.contact.id && chat.is(':visible') && $('#'+message.id).is(':visible')) {
         QB.chat.sendReadStatus({messageId: message.id, userId: id, dialogId: dialog_id});
