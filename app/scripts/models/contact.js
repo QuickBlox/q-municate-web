6e23b02a3d071431462275ca5146b8f8bbaa07c7
vladlukhanin <vladlukhanin@gmail.com>
2016-01-29 10:58:35 +0200

    avatar bug fix

--- app/scripts/models/contact.js
+++ app/scripts/models/contact.js
@@ -14,7 +14,6 @@ define(['config', 'quickblox'], function(QMCONFIG, QB) {
   Contact.prototype = {
 
     create: function(qbUser) {
-      console.log(qbUser);
       return {
         id: qbUser.id,
         full_name: qbUser.full_name,
@@ -23,7 +22,7 @@ define(['config', 'quickblox'], function(QMCONFIG, QB) {
         facebook_id: qbUser.facebook_id || null,
         blob_id: qbUser.blob_id || null,
         user_tags: qbUser.tag || qbUser.user_tags || null,
-        avatar_url: (qbUser.avatar_url || getAvatar(qbUser)).replace('http://', 'https://') || '',
+        avatar_url: (qbUser.avatar_url || getAvatar(qbUser)).replace('http://', 'https://') || QMCONFIG.defAvatar.url,
         status: qbUser.status || getStatus(qbUser),
         user_jid: qbUser.user_jid || QB.chat.helpers.getUserJid(qbUser.id, QMCONFIG.qbAccount.appId),
         custom_data: qbUser.custom_data || null
