f75d3e2c28caca48d3e4003690a1c6a05a077616
vladlukhanin <vladlukhanin@gmail.com>
2016-01-29 11:49:12 +0200

    avatar bug fix

--- app/scripts/views/dialog.js
+++ app/scripts/views/dialog.js
@@ -291,7 +291,7 @@ define([
       private_id = dialog.type === 3 ? dialog.occupants_ids[0] : null;
 console.log(dialog.occupants_ids[0]);
 console.log(contacts['8626122']);
-console.log(contacts[8626122]);
+console.log(contacts['8636106']);
       icon = private_id ? contacts[private_id].avatar_url : (dialog.room_photo || QMCONFIG.defAvatar.group_url);
       name = private_id ? contacts[private_id].full_name : dialog.room_name;
       status = roster[private_id] ? roster[private_id] : null;
