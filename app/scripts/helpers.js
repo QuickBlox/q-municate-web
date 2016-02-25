e2cb3449025c465f07a7983709aee7f529c2a0f3
vladlukhanin <vladlukhanin@gmail.com>
2016-02-25 10:33:12 +0200

    pull from develop before pull-request

--- app/scripts/helpers.js
+++ app/scripts/helpers.js
@@ -6,13 +6,11 @@ define(['jquery', 'config', 'QBNotification'], function($, QMCONFIG, QBNotificat
   var Helpers = {};
 
   Helpers.Notifications = {
-
     show: function(title, options) {
       var notify = new QBNotification(title, options);
 
       notify.show();
     },
-    
     getTitle: function(message, params) {
       var dialogs = params.dialogs,
           contacts = params.contacts,
