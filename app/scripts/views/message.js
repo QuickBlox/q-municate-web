5ad89cff93e38411d3de3affeb51707f248c9e8b
vladlukhanin <vladlukhanin@gmail.com>
2016-05-11 18:39:57 +0300

    attach geolocation (ready)

--- app/scripts/views/message.js
+++ app/scripts/views/message.js
@@ -52,9 +52,9 @@ define(['jquery', 'config', 'quickblox', 'underscore', 'minEmoji', 'Helpers', 't
             attachType = message.attachment && message.attachment['content-type'] || message.attachment && message.attachment.type || null,
             attachUrl = message.attachment && (QB.content.privateUrl(message.attachment.id) || message.attachment.url || null),
             geolocation = (message.latitude && message.longitude) ? {'latitude': message.latitude, 'longitude': message.longitude} : null,
-            geoCoords = (message.attachment && message.attachment.type === 'location') ? {'latitude': message.attachment['lat'], 'longitude': message.attachment['lng']} : null,
-            mapImage = geoCoords ? Location.getStaticMapUrl(geoCoords, {'size': [380, 200]}) : null,
-            mapLink = geoCoords ? Location.getMapUrl(geoCoords) : null,
+            geoCoords = (message.attachment && message.attachment.type === 'location') ? {'latitude': message.attachment.lat, 'longitude': message.attachment.lng} : null,
+            mapAttachImage = geoCoords ? Location.getStaticMapUrl(geoCoords, {'size': [380, 200]}) : null,
+            mapAttachLink = geoCoords ? Location.getMapUrl(geoCoords) : null,
             recipient = contacts[recipientId] || null,
             occupants_names = '',
             occupants_ids,
@@ -301,8 +301,8 @@ define(['jquery', 'config', 'quickblox', 'underscore', 'minEmoji', 'Helpers', 't
             html += '<div class="message-geo j-showlocation"></div></div>';
           } else if (attachType && attachType.indexOf('location') > -1) {
             html += '<div class="message-body">';
-            html += '<a class="open_googlemaps" href="'+mapLink+'" target="_blank">';
-            html += '<img id="attach_'+message.id+'" src="'+mapImage+'" alt="attach">';
+            html += '<a class="open_googlemaps" href="'+mapAttachLink+'" target="_blank">';
+            html += '<img id="attach_'+message.id+'" src="'+mapAttachImage+'" alt="attach">';
             html += '</a></div>';
             html += '</div><div class="message-info"><time class="message-time" data-time="'+message.date_sent+'">'+getTime(message.date_sent)+'</time>';
             html += '<div class="message-status is-hidden">Not delivered yet</div>';
