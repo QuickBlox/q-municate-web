c0501308d4e91841901b03342aa1aa07eb01047f
vladlukhanin <vladlukhanin@gmail.com>
2016-06-20 14:23:57 +0300

    User settings ready for merge with develop

--- app/scripts/models/settings.js
+++ app/scripts/models/settings.js
@@ -14,6 +14,8 @@ define([
     function Settings(app) {
         this.app = app;
         globalSettings = this;
+        
+        // default users settings
         globalSettings = {
             'messages_notify': true,
             'calls_notify':    true,
@@ -23,7 +25,9 @@ define([
 
     Settings.prototype = {
 
+        // set users settings from localStorage or create default (default - all is ON)
         setUp: function() {
+            // sets default settings if they are not there in the localStorage
             if (!localStorage['QM.settings']) {
                 localStorage.setItem('QM.settings', JSON.stringify(globalSettings));
 
@@ -32,16 +36,18 @@ define([
 
             var storageSettings = JSON.parse(localStorage['QM.settings']);
 
+            // copy settings from the localStorage['QM.settings']
             globalSettings.messages_notify = storageSettings.massages_notify;
             globalSettings.calls_notify    = storageSettings.calls_notify;
             globalSettings.sounds_notify   = storageSettings.sounds_notify;
 
+            // set checkbox position
             for (var key in storageSettings) {
                 $('#' + key)[0].checked = storageSettings[key];
-                console.info(key, $('#' + key)[0].checked);
             }
         },
 
+        // update users settings
         update: function(newStatus) {
             globalSettings[newStatus.id] = newStatus.checked;
             localStorage.setItem('QM.settings', JSON.stringify(globalSettings));
