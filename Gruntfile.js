5416e4262c584afb49a819af480711a7c2242ea9
vladlukhanin <vladlukhanin@gmail.com>
2016-06-30 12:42:03 +0300

    updated with atom-beautify

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,303 +4,303 @@ var SERVER_PORT = 9000;
 
 // # Globbing
 
+module.exports = function(grunt) {
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-module.exports = function (grunt) {
-  // show elapsed time at the end
-  require('time-grunt')(grunt);
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // configurable paths
+    var yeomanConfig = {
+        app: 'app',
+        dist: 'dist',
+        originalScriptTag: '<script src="scripts/main.js"></script>',
+        tmpScriptTag: '<script src="scripts/.build.js"></script>'
+    };
 
-  // configurable paths
-  var yeomanConfig = {
-    app: 'app',
-    dist: 'dist',
-    originalScriptTag: '<script src="scripts/main.js"></script>',
-    tmpScriptTag: '<script src="scripts/.build.js"></script>'
-  };
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        pkg: grunt.file.readJSON('bower.json'),
+        banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n',
 
-  grunt.initConfig({
-    yeoman: yeomanConfig,
-    pkg: grunt.file.readJSON('bower.json'),
-    banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-
-    clean: {
-      dev: ['.sass-cache', '.tmp', '<%= yeoman.app %>/.css'],
-      dist: ['.sass-cache', '.tmp', '<%= yeoman.app %>/.css',
-             '<%= yeoman.dist %>/scripts', '<%= yeoman.dist %>/styles', '<%= yeoman.dist %>/vendor'],
-      tmpBuild: ['<%= yeoman.app %>/scripts/.build.js']
-    },
-
-    compass: {
-      compile: {
-        options: {
-          cssDir: '<%= yeoman.app %>/.css',
-          sassDir: '<%= yeoman.app %>/styles',
-          javascriptsDir: '<%= yeoman.app %>/scripts',
-          imagesDir: '<%= yeoman.app %>/images',
-          noLineComments: true,
-          relativeAssets: true,
-          raw: 'preferred_syntax = :scss\n'
-        }
-      }
-    },
-
-    handlebars: {
-      compile: {
-        options: {
-          namespace: 'JST',
-          amd: true
+        clean: {
+            dev: ['.sass-cache', '.tmp', '<%= yeoman.app %>/.css'],
+            dist: ['.sass-cache', '.tmp', '<%= yeoman.app %>/.css',
+                '<%= yeoman.dist %>/scripts', '<%= yeoman.dist %>/styles', '<%= yeoman.dist %>/vendor'
+            ],
+            tmpBuild: ['<%= yeoman.app %>/scripts/.build.js']
         },
-        files: {
-          '.tmp/scripts/templates.js': ['<%= yeoman.app %>/scripts/templates/*.hbs']
-        }
-      }
-    },
 
-    bower: {
-      all: {
-        rjsConfig: '<%= yeoman.app %>/scripts/main.js',
-        options: {
-          exclude: ['jquery', 'modernizr', 'requirejs']
-        }
-      }
-    },
-
-    requirejs: {
-      dist: {
-        options: {
-          baseUrl: '<%= yeoman.app %>/scripts',
-          mainConfigFile: "<%= yeoman.app %>/scripts/main.js",
-          name: 'main',
-          optimize: 'none',
-          out: "<%= yeoman.app %>/scripts/.build.js",
-
-          paths: {
-            'templates': '.tmp/scripts/templates'
-          },
-
-          almond: false,
-          preserveLicenseComments: false
-        }
-      }
-    },
-
-    watch: {
-      options: {
-        spawn: false
-      },
-      css: {
-        files: ['<%= yeoman.app %>/styles/{,*/}*.scss'],
-        tasks: ['compass']
-      },
-      handlebars: {
-        files: [
-          '<%= yeoman.app %>/scripts/templates/*.hbs'
-        ],
-        tasks: ['handlebars']
-      }
-    },
-
-    useminPrepare: {
-      html: '<%= yeoman.app %>/index.html',
-      options: {
-        dest: '<%= yeoman.dist %>'
-      }
-    },
-
-    cssmin: {
-      options: {
-        banner: '<%= banner %>'
-      }
-    },
-
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      }
-    },
-
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,svg}',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
-
-    htmlmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>',
-          src: '*.html',
-          dest: '<%= yeoman.dist %>'
-        }]
-      }
-    },
-
-    rev: {
-      dist: {
-        files: {
-          src: [
-            '<%= yeoman.dist %>/scripts/{,*/}*.js',
-            '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/vendor/{,*/}*.js',
-          ]
-        }
-      }
-    },
-
-    usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
-      options: {
-        dirs: ['<%= yeoman.dist %>']
-      }
-    },
-
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>',
-          src: [
-            '*.{ico,png}',
-            'audio/{,*/}*.*'
-          ],
-          dest: '<%= yeoman.dist %>'
-        }]
-      }
-    },
-
-    connect: {
-      options: {
-        protocol: 'https',
-        port: grunt.option('port') || SERVER_PORT,
-        open: true,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      dev: {
-        options: {
-          base: [
-            '.tmp',
-            '<%= yeoman.app %>'
-          ]
-        }
-      },
-      dist: {
-        options: {
-          protocol: 'https',
-          base: '<%= yeoman.dist %>'
-        }
-      }
-    },
-
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      },
-      all: [
-        'Gruntfile.js',
-        '<%= yeoman.app %>/scripts/{,*/}*.js',
-        '!<%= yeoman.app %>/vendor/*'
-      ]
-    },
-
-    includereplace: {
-      prod: {
-        options: {
-          globals: {
-            appId: '13318',
-            authKey: 'WzrAY7vrGmbgFfP',
-            authSecret: 'xS2uerEveGHmEun'
-          }
+        compass: {
+            compile: {
+                options: {
+                    cssDir: '<%= yeoman.app %>/.css',
+                    sassDir: '<%= yeoman.app %>/styles',
+                    javascriptsDir: '<%= yeoman.app %>/scripts',
+                    imagesDir: '<%= yeoman.app %>/images',
+                    noLineComments: true,
+                    relativeAssets: true,
+                    raw: 'preferred_syntax = :scss\n'
+                }
+            }
         },
-        src: '<%= yeoman.app %>/configs/environment.js',
-        dest: '<%= yeoman.app %>/configs/main_config.js'
-      },
-      dev: {
-        options: {
-          globals: {
-            appId: '36125',
-            authKey: 'gOGVNO4L9cBwkPE',
-            authSecret: 'JdqsMHCjHVYkVxV'
-          }
+
+        handlebars: {
+            compile: {
+                options: {
+                    namespace: 'JST',
+                    amd: true
+                },
+                files: {
+                    '.tmp/scripts/templates.js': ['<%= yeoman.app %>/scripts/templates/*.hbs']
+                }
+            }
         },
-        src: '<%= yeoman.app %>/configs/environment.js',
-        dest: '<%= yeoman.app %>/configs/main_config.js'
-      },
-      local: {
-        src: '<%= yeoman.app %>/config.js',
-        dest: '<%= yeoman.app %>/configs/main_config.js'
-      }
-    }
-  });
 
-  var envTarget = grunt.option('env') || 'local';
+        bower: {
+            all: {
+                rjsConfig: '<%= yeoman.app %>/scripts/main.js',
+                options: {
+                    exclude: ['jquery', 'modernizr', 'requirejs']
+                }
+            }
+        },
 
-  grunt.registerTask('createDefaultTemplate', function () {
-    grunt.file.write('.tmp/scripts/templates.js', 'this.JST = this.JST || {};');
-  });
+        requirejs: {
+            dist: {
+                options: {
+                    baseUrl: '<%= yeoman.app %>/scripts',
+                    mainConfigFile: "<%= yeoman.app %>/scripts/main.js",
+                    name: 'main',
+                    optimize: 'none',
+                    out: "<%= yeoman.app %>/scripts/.build.js",
 
-  grunt.registerTask('createTmpScriptTag', function (rollBack) {
-    var path = yeomanConfig.app + '/index.html';
-    var indexFile = grunt.file.read(path);
-    if (typeof rollBack === 'undefined') {
-      grunt.file.write(path, indexFile.replace(yeomanConfig.originalScriptTag, yeomanConfig.tmpScriptTag));
-    } else {
-      grunt.file.write(path, indexFile.replace(yeomanConfig.tmpScriptTag, yeomanConfig.originalScriptTag));
-      grunt.task.run(['clean:tmpBuild']);
-    }
-  });
+                    paths: {
+                        'templates': '.tmp/scripts/templates'
+                    },
 
-  grunt.registerTask('server', function (target) {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve' + (target ? ':' + target : '')]);
-  });
+                    almond: false,
+                    preserveLicenseComments: false
+                }
+            }
+        },
 
-  grunt.registerTask('serve', function (target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
-    }
+        watch: {
+            options: {
+                spawn: false
+            },
+            css: {
+                files: ['<%= yeoman.app %>/styles/{,*/}*.scss'],
+                tasks: ['compass']
+            },
+            handlebars: {
+                files: [
+                    '<%= yeoman.app %>/scripts/templates/*.hbs'
+                ],
+                tasks: ['handlebars']
+            }
+        },
 
-    grunt.task.run([
-      'includereplace:' + envTarget,
-      'clean:dev',
-      'compass',
-      'createDefaultTemplate',
-      'handlebars',
-      'connect:dev',
-      'watch'
+        useminPrepare: {
+            html: '<%= yeoman.app %>/index.html',
+            options: {
+                dest: '<%= yeoman.dist %>'
+            }
+        },
+
+        cssmin: {
+            options: {
+                banner: '<%= banner %>'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            }
+        },
+
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.app %>/images',
+                    src: '{,*/}*.{png,jpg,jpeg,svg}',
+                    dest: '<%= yeoman.dist %>/images'
+                }]
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.app %>',
+                    src: '*.html',
+                    dest: '<%= yeoman.dist %>'
+                }]
+            }
+        },
+
+        rev: {
+            dist: {
+                files: {
+                    src: [
+                        '<%= yeoman.dist %>/scripts/{,*/}*.js',
+                        '<%= yeoman.dist %>/styles/{,*/}*.css',
+                        '<%= yeoman.dist %>/vendor/{,*/}*.js',
+                    ]
+                }
+            }
+        },
+
+        usemin: {
+            html: ['<%= yeoman.dist %>/{,*/}*.html'],
+            options: {
+                dirs: ['<%= yeoman.dist %>']
+            }
+        },
+
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.app %>',
+                    src: [
+                        '*.{ico,png}',
+                        'audio/{,*/}*.*'
+                    ],
+                    dest: '<%= yeoman.dist %>'
+                }]
+            }
+        },
+
+        connect: {
+            options: {
+                protocol: 'https',
+                port: grunt.option('port') || SERVER_PORT,
+                open: true,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            dev: {
+                options: {
+                    base: [
+                        '.tmp',
+                        '<%= yeoman.app %>'
+                    ]
+                }
+            },
+            dist: {
+                options: {
+                    protocol: 'https',
+                    base: '<%= yeoman.dist %>'
+                }
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                'Gruntfile.js',
+                '<%= yeoman.app %>/scripts/{,*/}*.js',
+                '!<%= yeoman.app %>/vendor/*'
+            ]
+        },
+
+        includereplace: {
+            prod: {
+                options: {
+                    globals: {
+                        appId: '13318',
+                        authKey: 'WzrAY7vrGmbgFfP',
+                        authSecret: 'xS2uerEveGHmEun'
+                    }
+                },
+                src: '<%= yeoman.app %>/configs/environment.js',
+                dest: '<%= yeoman.app %>/configs/main_config.js'
+            },
+            dev: {
+                options: {
+                    globals: {
+                        appId: '36125',
+                        authKey: 'gOGVNO4L9cBwkPE',
+                        authSecret: 'JdqsMHCjHVYkVxV'
+                    }
+                },
+                src: '<%= yeoman.app %>/configs/environment.js',
+                dest: '<%= yeoman.app %>/configs/main_config.js'
+            },
+            local: {
+                src: '<%= yeoman.app %>/config.js',
+                dest: '<%= yeoman.app %>/configs/main_config.js'
+            }
+        }
+    });
+
+    var envTarget = grunt.option('env') || 'local';
+
+    grunt.registerTask('createDefaultTemplate', function() {
+        grunt.file.write('.tmp/scripts/templates.js', 'this.JST = this.JST || {};');
+    });
+
+    grunt.registerTask('createTmpScriptTag', function(rollBack) {
+        var path = yeomanConfig.app + '/index.html';
+        var indexFile = grunt.file.read(path);
+        if (typeof rollBack === 'undefined') {
+            grunt.file.write(path, indexFile.replace(yeomanConfig.originalScriptTag, yeomanConfig.tmpScriptTag));
+        } else {
+            grunt.file.write(path, indexFile.replace(yeomanConfig.tmpScriptTag, yeomanConfig.originalScriptTag));
+            grunt.task.run(['clean:tmpBuild']);
+        }
+    });
+
+    grunt.registerTask('server', function(target) {
+        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+        grunt.task.run(['serve' + (target ? ':' + target : '')]);
+    });
+
+    grunt.registerTask('serve', function(target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'includereplace:' + envTarget,
+            'clean:dev',
+            'compass',
+            'createDefaultTemplate',
+            'handlebars',
+            'connect:dev',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('build', [
+        'jshint',
+        'includereplace:' + envTarget,
+        'clean:dist',
+        'compass',
+        'createDefaultTemplate',
+        'handlebars',
+        'requirejs',
+        'createTmpScriptTag',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'newer:imagemin',
+        'htmlmin',
+        'rev',
+        'usemin',
+        'newer:copy',
+        'createTmpScriptTag:rollBack'
     ]);
-  });
 
-  grunt.registerTask('build', [
-    'jshint',
-    'includereplace:' + envTarget,
-    'clean:dist',
-    'compass',
-    'createDefaultTemplate',
-    'handlebars',
-    'requirejs',
-    'createTmpScriptTag',
-    'useminPrepare',
-    'concat',
-    'cssmin',
-    'uglify',
-    'newer:imagemin',
-    'htmlmin',
-    'rev',
-    'usemin',
-    'newer:copy',
-    'createTmpScriptTag:rollBack'
-  ]);
-
-  grunt.registerTask('default', ['build']);
-  grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('test', ['jshint']);
 
 };
--- README.md
+++ README.md
@@ -1,83 +1,92 @@
 # Q-municate
 
-[Q-municate](https://q-municate.com)  is an open source code chat application with wide range of communication features available (such as one-to-one messaging, group chat messaging, file transfers, notifications, Facebook signup and audio/video calls).
+[Q-municate](https://q-municate.com) is an open source code chat application with wide range of communication features available (such as one-to-one messaging, group chat messaging, file transfers, notifications, Facebook signup and audio/video calls).
 
-We are please to present you with an out of the box chat application. You can customize this application depending of your needs.
-[QuickBlox](https://quickblox.com) is used for the backend.
+We are please to present you with an out of the box chat application. You can customize this application depending of your needs. [QuickBlox](https://quickblox.com) is used for the backend.
 
-Find the source code and more information about Q-municate in our Developers section: http://quickblox.com/developers/q-municate
+Find the source code and more information about Q-municate in our Developers section: <http://quickblox.com/developers/q-municate>
 
 ## Q-municate Web
+
 This description was written by the QuickBlox Web team in order to fully explain how you can build a communication javascript app with the Quickblox API.
 
-Q-municate is designed for all developers, including beginners, as we move from simple to more complex implementation.
-Enjoy and please get in touch if you need any assistance.
+Q-municate is designed for all developers, including beginners, as we move from simple to more complex implementation. Enjoy and please get in touch if you need any assistance.
 
 ## Live demo
-* Web - [Live app](https://qm.quickblox.com/)
-* iOS - [App Store](https://itunes.apple.com/us/app/q-municate/id909698517?mt=8)
-* Android - [Google Play](https://play.google.com/store/apps/details?id=com.quickblox.q_municate)
-* WP - [Microsoft Store](https://www.microsoft.com/en-us/store/apps/q-municate-messenger/9nblggh5jqnb)
+
+- Web - [Live app](https://qm.quickblox.com/)
+- iOS - [App Store](https://itunes.apple.com/us/app/q-municate/id909698517?mt=8)
+- Android - [Google Play](https://play.google.com/store/apps/details?id=com.quickblox.q_municate)
+- WP - [Microsoft Store](https://www.microsoft.com/en-us/store/apps/q-municate-messenger/9nblggh5jqnb)
 
 ## Software Environment
-* The web component is based on the QuickBlox platform
-* The user interface is in English
-* No crashes or exceptions are allowed
-* The app immediately reacts to any user action or give notifications about each action which requires time to be processed
-* User information is kept safely and securely.
 
-* User's password is encoded and kept in the local storage.
-* The App should work correctly in the following browsers:
-   - Chrome 13+
-   - Firefox 4+
-   - Opera 17+
-   - Safari 6.1+ (without audio, video calls)  
-   - IE 10+ (without audio, video calls)
+- The web component is based on the QuickBlox platform
+- The user interface is in English
+- No crashes or exceptions are allowed
+- The app immediately reacts to any user action or give notifications about each action which requires time to be processed
+- User information is kept safely and securely.
+
+- User's password is encoded and kept in the local storage.
+
+- The App should work correctly in the following browsers:
+
+  - Chrome 13+
+  - Firefox 4+
+  - Opera 17+
+  - Safari 6.1+ (without audio, video calls)
+  - IE 10+ (without audio, video calls)
 
 Q-municate is a fully fledged chat application using the Quickblox API.
 
 ## How to build your own Chat app
+
 You can build your own chat app using Q-municate as a basis in just 5 minutes! All you need is to:
 
-#### 1. Register a QuickBlox account
-If you don't have one yet, just register your account at [https://admin.quickblox.com/register](https://admin.quickblox.com/register)
+### 1\. Register a QuickBlox account
 
-<img src="http://quickblox.com/developers//images/7/70/Register_your_account.jpg" />&nbsp;
+If you don't have one yet, just register your account at <https://admin.quickblox.com/register>
+
+![](http://quickblox.com/developers//images/7/70/Register_your_account.jpg)
 
 You can read more about how to create an account [here](https://quickblox.com/developers/5_Minute_Guide#Create_your_QuickBlox_account).
 
-#### 2. Login to QuickBlox admin panel
-Login into admin panel at [https://admin.quickblox.com/signin](https://admin.quickblox.com/signin)
-You can read more about how to sign in to admin panel [here](https://quickblox.com/developers/5_Minute_Guide#Sign_in_to_the_admin_panel).
+### 2\. Login to QuickBlox admin panel
 
-#### 3. Create a new app.
-Click on the "Add new application" button after "Log in": [https://admin.quickblox.com](https://admin.quickblox.com) and fill an application form. If you don't have an app yet, you can follow a detailed guide about app creation at [here](https://quickblox.com/developers/5_Minute_Guide#Create_an_app_in_the_admin_panel).
+Login into admin panel at <https://admin.quickblox.com/signin> You can read more about how to sign in to admin panel [here](https://quickblox.com/developers/5_Minute_Guide#Sign_in_to_the_admin_panel).
 
-#### 4. Get app credentials
+### 3\. Create a new app.
+
+Click on the "Add new application" button after "Log in": <https://admin.quickblox.com> and fill an application form. If you don't have an app yet, you can follow a detailed guide about app creation at [here](https://quickblox.com/developers/5_Minute_Guide#Create_an_app_in_the_admin_panel).
+
+### 4\. Get app credentials
+
 Click on the app title in the list to reveal the app details. You are going to need App ID, Authorization key and Authorization secret later in our Q-municate source code.
 
-<img src="https://quickblox.com/developers//images/e/e3/Info_about_app.jpg" />&nbsp;
+![](https://quickblox.com/developers//images/e/e3/Info_about_app.jpg)
 
-#### 5. Setup environment
+### 5\. Setup environment
+
 If you want to build your own app using Q-municate as a base, please do the following:
 
 1. Install [nodeJS](https://nodejs.org/en/download/) and [Ruby](https://www.ruby-lang.org/en/downloads) before start.
 2. Download the project from [GitHub](https://github.com/QuickBlox/q-municate-web/archive/master.zip).
-3. Run <code>npm install -g bower</code> in your terminal as an administrator.
-4. Run <code>gem install compass</code> in your terminal as an administrator.
-5. Run <code>npm install -g grunt-cli</code> in your terminal as an administrator.
-6. Run <code>bower install</code> to install all additional packages in your terminal.
-7. Run <code>npm install</code> to install all additional packages in your terminal as an administrator.
-8. Copy the credentials (App ID, Authorization key, Authorization secret) and your Facebook App ID ([How to generate and save Facebook application ID](https://quickblox.com/developers/How_to_generate_and_save_Facebook_application_ID)) into your Q-municate project code in ```config.js```.
+3. Run `npm install -g bower` in your terminal as an administrator.
+4. Run `gem install compass` in your terminal as an administrator.
+5. Run `npm install -g grunt-cli` in your terminal as an administrator.
+6. Run `bower install` to install all additional packages in your terminal.
+7. Run `npm install` to install all additional packages in your terminal as an administrator.
+8. Copy the credentials (App ID, Authorization key, Authorization secret) and your Facebook App ID ([How to generate and save Facebook application ID](https://quickblox.com/developers/How_to_generate_and_save_Facebook_application_ID)) into your Q-municate project code in `config.js`.
 
-<img src="http://quickblox.com/developers//images/9/95/Js_qm_project.png" />&nbsp;
+![](http://quickblox.com/developers//images/9/95/Js_qm_project.png)
 
-#### 6. Build and run
-1. Run <code>grunt build</code> or <code>grunt</code> (build with jshint verification) in your terminal to build Q-municate (q-municate-web/dist).
-2. Run <code>grunt serve</code> in your terminal to open Q-municate in a browser window (https://localhost:9000).
+### 6\. Build and run
 
-<img src="http://quickblox.com/developers//images/7/7b/Gruntserve.jpg" />&nbsp;
+1. Run `grunt build` or `grunt` (build with jshint verification) in your terminal to build Q-municate (q-municate-web/dist).
+2. Run `grunt serve` in your terminal to open Q-municate in a browser window (<https://localhost:9000>).
+
+![](http://quickblox.com/developers//images/7/7b/Gruntserve.jpg)
 
 # License
-Apache License
-Version 2.0
+
+Apache License Version 2.0
