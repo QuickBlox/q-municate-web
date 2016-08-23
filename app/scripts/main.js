1711555eb71ccb69281077530b0ce4da5a46ebcb
vladlukhanin <vladlukhanin@gmail.com>
2016-08-23 15:42:59 +0300

    check FB

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -153,7 +153,7 @@ requirejs([
         }
 
         APP = new QM();
-        setTimeout(APP.init(), 1000);
+        setTimeout(APP.init(), 3000);
 
         $.getScript('https://cdn.flurry.com/js/flurry.js', function() {
             FlurryAgent.startSession('P8NWM9PBFCK2CWC8KZ59');
