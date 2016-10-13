8120729d443b41dded96fe95704dd4ccebf3cdb4
vladlukhanin <vladlukhanin@gmail.com>
2016-10-13 13:00:38 +0300

    version v.1.8.0

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -115,7 +115,7 @@ requirejs([
     // Application initialization
     $(function() {
         // set Q-MUNICATE version
-        $('.j-appVersion').html('v. 1.7.16');
+        $('.j-appVersion').html('v. 1.8.0');
 
         // facebook sdk
         FB.init({
