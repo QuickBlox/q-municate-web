9e2f22fdad1bc465dbf2c3063a4f8432026c51c5
vladlukhanin <vladlukhanin@gmail.com>
2016-12-02 17:41:03 +0200

    updated styles for history preloader

--- app/scripts/views/dialog.js
+++ app/scripts/views/dialog.js
@@ -126,7 +126,8 @@ define([
 
             var spinnerBlock;
             if (isAjaxDownloading) {
-                spinnerBlock = '<div class="message message_service"><div class="popup-elem spinner_bounce is-empty is-ajaxDownload">';
+                spinnerBlock = '<div class="message message_service msg_preloader">' +
+                    '<div class="popup-elem spinner_bounce is-empty is-ajaxDownload">';
             } else if (groupchat) {
                 spinnerBlock = '<div class="popup-elem spinner_bounce is-creating">';
             } else {
