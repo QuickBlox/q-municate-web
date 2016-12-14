934d4539ddd4b5078c2e591992aff6a068de7fc3
vladlukhanin <vladlukhanin@gmail.com>
2016-12-14 13:02:56 +0200

    redirect users to http://q-municate.com/ for mobile browsers

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -120,7 +120,7 @@ requirejs([
     // Application initialization
     $(function() {
         // set Q-MUNICATE version
-        $('.j-appVersion').html('v. 1.9.7');
+        $('.j-appVersion').html('v. 1.9.8');
 
         // facebook sdk
         FB.init({
