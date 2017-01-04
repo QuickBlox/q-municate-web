af9b9f6f60c368c36deb3e3440e994428e562d5c
vladlukhanin <vladlukhanin@gmail.com>
2017-01-04 17:55:45 +0200

    1.9.17

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -121,7 +121,7 @@ requirejs([
     // Application initialization
     $(function() {
         // set Q-MUNICATE version
-        $('.j-appVersion').html('v. 1.9.16');
+        $('.j-appVersion').html('v. 1.9.17');
 
         // facebook sdk
         FB.init({
