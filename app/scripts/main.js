bff2602f4f361998bb5830ce564240c3d6a2459c
vladlukhanin <vladlukhanin@gmail.com>
2017-04-28 15:50:10 +0300

    Updated audio/video call's status (styles)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -113,7 +113,7 @@ requirejs([
     // Application initialization
     $(function() {
         // set Q-MUNICATE version
-        $('.j-appVersion').html('v. 1.11.8');
+        $('.j-appVersion').html('v. 1.11.9');
 
         $.ajaxSetup({cache: true});
 
