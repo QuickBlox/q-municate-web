d9b43cc59fafd0682089791c490a10455254f974
Vladyslav Lukhanin <vladislav.lukhanin@quickblox.com>
2017-08-04 17:36:02 +0300

    Updated design (#114)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -111,7 +111,7 @@ requirejs([
     // Application initialization
     $(function() {
         // set Q-MUNICATE version
-        $('.j-appVersion').html('v. 1.12.4');
+        $('.j-appVersion').html('v. 1.12.5');
 
         $.ajaxSetup({cache: true});
 
